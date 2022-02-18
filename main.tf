dd7b4cebe77e1ddf8365f5f58201c99a4f2fd4ba
Harshada Deokar <46487473+HarshadaDeokar317@users.noreply.github.com>
2022-02-18 15:22:39 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    
+    profile = "default"
+    region = "us-west-2"
+
+}
+
+resource "aws_s3_bucket" "statefileterraform" {
+    bucket = "statefilestoreterraform" 
+
+    
+    
+}
+
+output "bucket_name" {
+
+    value = aws_s3_bucket.statefileterraform
+
+  
+}
