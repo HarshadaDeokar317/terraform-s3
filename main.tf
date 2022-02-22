41f9007870cb1b4abf09b3147c72d24e8258b4b4
Harshada Deokar <46487473+HarshadaDeokar317@users.noreply.github.com>
2022-02-22 10:27:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
 provider "aws" {
-      access_key = var.TF_AWS_ACCESS_KEY_ID
-      secret_key = var.TF_AWS_SECRET_ACCESS_KEY
+    
       profile = "default"
       region = "us-west-2"
  
 }
 
-variable "TF_AWS_ACCESS_KEY_ID" {
- 
-}
 
-variable "TF_AWS_SECRET_ACCESS_KEY" {
- 
-}
 resource "aws_s3_bucket" "statefileterraform" {
     bucket = "statefilestoreterraform" 
 
